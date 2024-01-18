ccae6d31aea1b8f3f00d7aa8ac6146feb44f124b
hardikkum444 <hardikkumawat444@gmail.com>
2024-01-19 01:33:23 +0530

    simple java program to add two numbers

---
+++ addition.java
@@ -0,0 +1,12 @@
+//simple java program to add two numbers
+
+public class addition {
+    public static void main(String[] args) {
+        int a = 19;
+        int b = 72;
+
+        int c = a + b;
+
+        System.out.println(c);
+    }
+}
