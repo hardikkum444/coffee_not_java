ee899d728e15da34d0e3a301eaea80d6ae69c55e
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2024-02-06 16:17:52 +0530

    revising args in the form of multiplying

---
+++ Arg.java
@@ -0,0 +1,25 @@
+//revising args in the form of multiplying
+
+public class Arg{
+
+	public static void main (String[] args){
+
+		if (args.length>0){
+
+			int multiple = 1;
+			for(int i=0;i<args.length;i++){
+
+					Integer n1 =  Integer.parseInt(args[i]);
+					multiple = multiple*n1;
+
+			}
+
+			System.out.println("And your product is--> "+multiple);
+		}
+		else{
+
+			System.out.println("Nothing given as an arg");
+		}
+	}
+
+}
