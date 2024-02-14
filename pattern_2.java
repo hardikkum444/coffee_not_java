57ee19b9d81104d2f45b9dbd625cbc02123d9806
hardikkum444 <hardikkumawat444@gmail.com>
2024-02-14 23:54:11 +0530

    reversing a pattern

---
+++ pattern_2.java
@@ -0,0 +1,16 @@
+//printing the pattern in reverse order
+
+public class pattern_2{
+
+    public static void main(String[] args){
+
+        for(int i=5;i>0;i--){
+
+            for(int j = i;j>0;j--){
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
