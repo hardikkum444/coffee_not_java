16e22746ab35422abc5ecf0aa9eaba5405b84269
hardikkum444 <hardikkumawat444@gmail.com>
2024-02-17 10:29:23 +0530

    ternary

---
+++ ternary_op.java
@@ -0,0 +1,11 @@
+public class Main{
+    
+    public static void main(String[] args){
+        
+        int n = 90;
+        
+        boolean ans = (n>80) ? true : false;
+        
+        System.out.println(ans);
+    }
+}
