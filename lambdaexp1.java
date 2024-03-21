48d8d7e455a729af3d53a35d2f8fe8684bb1c941
hardikkum444 <hardikkumawat444@gmail.com>
2024-03-21 16:46:16 +0530

    using lambda exp's

--- lambdaexp1.java
+++ lambdaexp1.java
@@ -1,2 +1,25 @@
 //dynamic method dispatching
+//demonstrating the usage of lambda expression in java
 
+@FunctionalInterface
+interface open{
+
+    void getName(int a);
+}
+
+public class lambdaexp1{
+
+    public static void main(String[] args){
+
+        int a = 8;
+        open obj = (b)->{ //no need to write "int b" also
+        
+            System.out.println("Usage of lambda expressions in java "+a);
+        };
+        //or a one liner like this:
+        //open obj = ()->{System.out.println("blah blah blah")};
+
+        obj.getName(a);
+    }
+
+}
