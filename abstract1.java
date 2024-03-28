1abe17f81ad269ad85c53bd70b1234a00cd15e58
hardikkum444 <hardikkumawat444@gmail.com>
2024-03-28 18:07:56 +0530

    abstract classes in java and overriding

---
+++ abstract1.java
@@ -0,0 +1,44 @@
+// one very important thing to remember in abstract class is that overiding is used
+
+// now in normal ovrriding, you can change the parameters and the return type of the function
+
+// but when it comes to abstract classes, you cant change the config of the function that you have set in your abstract class i.e you cannot change the parameters or return type from what you have declared in the abstract class
+
+public class abstract1 {
+    
+    public static void main(String[] args){
+        
+        System.out.println("this is working");
+        
+        derived1 hardik = new derived1();
+        
+        hardik.greet();
+        hardik.name("hardik");
+        hardik.age(19);
+    }
+}
+
+abstract class steal {
+    public void greet(){
+        System.out.println("buenos dias mi amigo");
+    }
+    
+    public abstract void name(String name);
+    
+    public abstract void age(int age);
+}
+
+class derived1 extends steal {
+    
+    @Override
+    public void name(String name){
+        
+        System.out.println("hello "+name);
+    }
+    
+    @Override
+    public void age(int age){
+        
+        System.out.println("your age is "+age);
+    }
+}
