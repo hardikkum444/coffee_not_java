17261f0200f89d3a8dcbc8764acd91201a5d6ce4
hardikkum444 <hardikkumawat444@gmail.com>
2024-03-29 23:00:03 +0530

    building strings

---
+++ stringbuilder1.java
@@ -0,0 +1,29 @@
+// this isnt relevant but is quite cool
+// when input is given in string and you want to store it in an array 
+
+
+// public class stringbuilder1{
+
+//     public static void main(String[] args){
+    
+//         String inputString = "mandom ting browsing innit locker";
+
+//         String[] arr = inputString.split(" ");
+
+//         for(String obj:arr){
+        
+//             System.out.println(obj);
+//         }
+//     }
+// }
+
+
+// now using STRINGBUILDER
+
+public class stringbuilder1 {
+
+    public static void main (String[] args){
+    
+        
+    }
+}
