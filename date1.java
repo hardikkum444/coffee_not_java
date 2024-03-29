259abad1d59585a281f20d40736a5f73e0ef74df
hardikkum444 <hardikkumawat444@gmail.com>
2024-03-29 23:01:23 +0530

    getting started

---
+++ date1.java
@@ -0,0 +1,16 @@
+// 1 january 1970
+// simple java date code
+
+import java.util.*;
+
+public class date1{
+
+    public static void main(String[] args){
+    
+        // System.out.println(System.currentTimeMillis());
+        
+        Date d = new Date(2023,03,30);
+        System.out.println(d);
+        System.out.println(d.toString());
+    }
+}
