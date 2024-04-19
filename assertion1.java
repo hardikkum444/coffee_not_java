3321bd05533778793a6601a24284b6cd032ae3a9
hardikkum444 <hardikkumawat444@gmail.com>
2024-04-19 13:02:35 +0530

    assertions in java

---
+++ assertion1.java
@@ -0,0 +1,13 @@
+// use java -ea fileName for assertion
+
+public class assertion1{
+
+    public static void main(String[] args){
+
+        int age = 18;
+
+        assert age>=18 : "Age must be greater than or equal to 18 to vote";
+
+            System.out.println("you can vote");
+    }
+}
