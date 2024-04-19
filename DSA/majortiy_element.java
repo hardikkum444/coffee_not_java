453191426957b263112383064851751fda6299a6
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2024-04-19 11:28:29 +0530

    leetcode (easy)

---
+++ DSA/majortiy_element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int majority = nums[0];
+        int count = 1;
+
+        for(int i =0;i<nums.length;i++){
+
+            if(majority == nums[i]){
+                count++;
+
+            }else{
+                count--;
+            }
+
+            if(count==0){
+                majority = nums[i];
+                count = 1;
+            }
+        }
+
+        return majority;
+        
+    }
+}
