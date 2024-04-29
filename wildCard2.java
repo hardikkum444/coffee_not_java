761ee16930992988e5f0a034a3b3eb63cb9695d5
hardikkum444 <hardikkumawat444@gmail.com>
2024-04-29 21:44:42 +0530

    wild card and template

---
+++ wildCard2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class wildCard2{
+
+    public static <T extends Number> Number sumList(List<T>list){
+        double sum = 0;
+        for(Number num : list){
+            sum+=num.doubleValue();
+        }
+
+        if(list.get(0) instanceof Integer){
+            return (int) sum;
+        }else if(list.get(0) instanceof Double){
+            return sum;
+        }else{
+            return 0.00;
+        }
+    }
+
+    public static void main(String[] args){
+        
+        System.out.println("Give size of arrayList --> ");
+        Scanner scan = new Scanner(System.in);
+
+        int size = scan.nextInt();
+        System.out.println("Entries for double list --> ");
+        
+        ArrayList<Double> arr = new ArrayList<>();
+        ArrayList<Integer> arr1 = new ArrayList<>();
+
+        for(int i = 0;i<size;i++){
+            double temp = scan.nextDouble();
+            arr.add(temp);
+        }
+
+        System.out.println("for double arraylist --> "+sumList(arr));
+
+        for(int i = 0;i<size;i++){
+
+            int temp = scan.nextInt();
+            arr1.add(temp);
+        }
+        System.out.println("for integer arraylist --> "+sumList(arr1));
+
+    }
+}
