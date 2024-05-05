d5577ec4ada01676f64c4145b7a6a8c1b452ae6e
hardikkum444 <hardikkumawat444@gmail.com>
2024-05-05 12:52:42 +0530

    added more stuff

--- prac_101/date1.java
+++ prac_101/date1.java
@@ -9,3 +9,19 @@ class Main {
         System.out.println(ans);
     }
 }
+
+//printing the local date and time
+import java.util.*;
+import java.time.*;
+
+class main{
+    public static void main(String[] args){
+
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+    }
+}
+
+// IMP
+// LocalDate format = 2024-05-05
+// Date d = new Date() format = Sun May 05 07:21:09 GMT 2024
